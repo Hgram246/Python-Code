b5d6ba43db231cadd244c7915c2056f11c20f7a4
Hgram246 <hgram246@gmail.com>
2024-09-18 22:39:50 -0700

    pulls min and max timestamps and stores them as st rings.

--- screener/stockscreener.py
+++ screener/stockscreener.py
@@ -5,13 +5,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.signal import argrelextrema
 
-stocks = ["BTC-USD"]
+stocks = ["BTC-USD", "ETH", "SOL", "TRX"]
 
 for stock in stocks:   
-    df = yf.download(stock,start=pd.to_datetime('today') - timedelta(1), interval='1m')
-    
-    df.to_csv("Data{stock}.csv")
-
+    df = yf.download(stock,start=pd.to_datetime('today') - timedelta(6), interval='1m')
 
     #I don't know how this works, but it returns the right things.
     #I lifted this from stack overflow, link below
@@ -25,8 +22,29 @@ for stock in stocks:
     df['min'] = df.iloc[argrelextrema(df.High.values, np.less_equal, order=n)[0]]['High']
     df['max'] = df.iloc[argrelextrema(df.High.values, np.greater_equal, order=n)[0]]['High']
 
+    max = df[(df['max']>0)].select_dtypes(include=['datetime'])
+    min = df[(df['min']>0)].select_dtypes(include=['datetime'])
+
+    maxlist = max.index.tolist()
+    minlist = min.index.tolist()
+    
+    
+    file = open("data/"+stock+'(max).txt', 'w')
+    for item in maxlist:
+        file.write(str(item)+'\n')
+    file.close()
+    file = open("data/"+stock+'(min).txt', 'w')
+    for item in minlist:
+        file.write(str(item)+'\n')
+    file.close()
+    
+    
+
+
     plt.title(stock+" Stock Data")
     plt.scatter(df.index, df['min'], c='r')
     plt.scatter(df.index, df['max'], c='g')
     plt.plot(df.index, df['High'])
     plt.show()
+    
+    
