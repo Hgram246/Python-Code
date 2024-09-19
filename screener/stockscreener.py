f968a509d35c96d379a25570f9cd004d3df4a1c8
Hgram246 <hgram246@gmail.com>
2024-09-18 19:25:06 -0700

    commit

--- screener/stockscreener.py
+++ screener/stockscreener.py
@@ -1,23 +1,32 @@
 import yfinance as yf
 import pandas as pd
 from datetime import timedelta
-from IPython.display import display
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.signal import argrelextrema
 
-stocks = ["INTC", "NKE", "AAPL", "AMZN", "MCD"]
+stocks = ["BTC-USD"]
 
 for stock in stocks:   
-    df = yf.download(stock,start=pd.to_datetime('today') - timedelta(1), interval='5m')
+    df = yf.download(stock,start=pd.to_datetime('today') - timedelta(1), interval='1m')
     
-    n = 5
+    df.to_csv("Data{stock}.csv")
+
+
+    #I don't know how this works, but it returns the right things.
+    #I lifted this from stack overflow, link below
+    #https://stackoverflow.com/questions/48023982/pandas-finding-local-max-and-min
+    #  <3
+
+    #the higher n is, the less local min/max it will find.
+    
+    n = 15
 
     df['min'] = df.iloc[argrelextrema(df.High.values, np.less_equal, order=n)[0]]['High']
     df['max'] = df.iloc[argrelextrema(df.High.values, np.greater_equal, order=n)[0]]['High']
 
+    plt.title(stock+" Stock Data")
     plt.scatter(df.index, df['min'], c='r')
     plt.scatter(df.index, df['max'], c='g')
     plt.plot(df.index, df['High'])
     plt.show()
-
