1143ba96d9f1e258e49dd962485a4e4eefd73b89
Hgram246 <hgram246@gmail.com>
2024-09-18 17:14:17 -0700

    commit

--- screener/stockscreener.py
+++ screener/stockscreener.py
@@ -3,13 +3,21 @@ import pandas as pd
 from datetime import timedelta
 from IPython.display import display
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.signal import argrelextrema
 
-stocks = ["INTC"]
+stocks = ["INTC", "NKE", "AAPL", "AMZN", "MCD"]
 
 for stock in stocks:   
     df = yf.download(stock,start=pd.to_datetime('today') - timedelta(1), interval='5m')
-    plt.plot(df["High"].index.values,df['High'])
-    plt.title(stock+" Stock Data")
-    plt.legend(["Line"])
+    
+    n = 5
+
+    df['min'] = df.iloc[argrelextrema(df.High.values, np.less_equal, order=n)[0]]['High']
+    df['max'] = df.iloc[argrelextrema(df.High.values, np.greater_equal, order=n)[0]]['High']
+
+    plt.scatter(df.index, df['min'], c='r')
+    plt.scatter(df.index, df['max'], c='g')
+    plt.plot(df.index, df['High'])
     plt.show()
 
